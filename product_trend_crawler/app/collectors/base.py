72722b2caac10ffddc7f83b51079f3381a8f3d88
fislirami97-glitch <fislirami97@gmail.com>
2026-09-24 03:18:16 +0200

    Delete product_trend_crawler directory

--- product_trend_crawler/app/collectors/base.py
+++
@@ -1,8 +0,0 @@
-from abc import ABC, abstractmethod
-from typing import Iterable
-from app.schemas import DiscoveredItem
-
-class Collector(ABC):
-    @abstractmethod
-    async def search(self, query: str, domain: str, limit: int = 10) -> Iterable[DiscoveredItem]:
-        raise NotImplementedError
